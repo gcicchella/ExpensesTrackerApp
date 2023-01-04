0de4430b23fbb7ba657fc0c486bc58cbdc895132
g.cicchella <giuseppecicchella28@gmail.com>
2023-01-04 11:28:28 +0100

    diversificato click (Add o Edit)

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
 import {Text} from "react-native";
+import {useLayoutEffect} from "react";
 
-function ManageExpense(){
+function ManageExpense({route, navigation}){
+    const editExpenseId = route.params?.expenseId;
+    const isEditing = !!editExpenseId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        });
+    }, [navigation, isEditing]);
+
     return (
         <Text>ManageExpense Screen</Text>
     );
