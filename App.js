4e6fd8e34a515442c3a136b3e00eacc7ecb7a5c9
g.cicchella <giuseppecicchella28@gmail.com>
2023-01-04 11:09:00 +0100

    aggiunta bottone + e navigation

--- App.js
+++ App.js
@@ -9,18 +9,30 @@ import AllExpenses from "./screens/AllExpenses";
 import ManageExpense from "./screens/ManageExpense";
 
 import {GlobalStyles} from "./constants/styles";
+import IconButton from "./UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview(){
   return (
-      <BottomTabs.Navigator screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.primary500},
-          headerTintColor: 'white',
-          tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500},
-          tabBarActiveTintColor: GlobalStyles.colors.accent500
-      }}>
+      <BottomTabs.Navigator
+          screenOptions={({navigation}) => ({
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500},
+            headerTintColor: 'white',
+            tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500},
+            tabBarActiveTintColor: GlobalStyles.colors.accent500,
+            headerRight: ({tintColor}) =>
+                <IconButton
+                    icon="add"
+                    size={24}
+                    color={tintColor}
+                    onPress={() => {
+                        navigation.navigate('ManageExpense');
+                    }}
+                />
+         })}
+      >
           <BottomTabs.Screen
               name="RecentExpenses"
               component={RecentExpenses}
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -2,10 +2,17 @@ import {Pressable, Text, View, StyleSheet} from "react-native";
 import {GlobalStyles} from "../constants/styles";
 
 import {getFormattedDate} from "../util/date";
+import {useNavigation} from "@react-navigation/native";
 
 function ExpenseItem({description, amount, date}) {
+
+    const navigation = useNavigation();
+    function exprensePressHandler() {
+        navigation.navigate('ManageExpense');
+    }
+
     return (
-        <Pressable>
+        <Pressable onPress={exprensePressHandler} style={({pressed}) => pressed && styles.pressed}>
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.description]}> {description} </Text>
@@ -22,6 +29,9 @@ function ExpenseItem({description, amount, date}) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+    pressed: {
+      opacity: 0.75
+    },
     expenseItem: {
        padding: 12,
        marginVertical: 8,
