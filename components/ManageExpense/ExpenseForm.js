c2fbe657a28680d563172f066ae7f75285e2d8cc
g.cicchella <giuseppecicchella28@gmail.com>
2023-02-10 10:08:14 +0100

    Aggiunta stile al file Input

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -3,11 +3,26 @@ import {View} from "react-native";
 import Input from './Input'
 
 function ExpenseForm(){
+    function amountChangeHandler() {
+
+    }
+
     return (
         <View>
-            <Input label="Amount"/>
-            <Input label="Date"/>
-            <Input label="Description"/>
+            <Input label="Amount" textInputConfig={{
+               keyboardType: 'decimal-pad',
+                onChangeText: amountChangeHandler,
+            }} />
+            <Input label="Date" textInputConfig={{
+                placeholder: 'YYYY-MM-DD',
+                maxLength: 10,
+                onChangeText: () => {}
+            }} />
+            <Input label="Description" textInputConfig={{
+                multiline: true,
+                // autoCapitalize: 'none'
+                // autoCorrect: false
+            }}/>
         </View>
     );
 }
